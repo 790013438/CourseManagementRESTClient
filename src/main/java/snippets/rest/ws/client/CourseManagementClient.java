9c33cdc3ec7eb4e88a254e28b6631bb8f2bac109
790013438 <790013438@qq.com>
2017-10-07 20:41:09 +0800

    creating a java client for the REST GET web service

--- src/main/java/snippets/rest/ws/client/CourseManagementClient.java
+++ src/main/java/snippets/rest/ws/client/CourseManagementClient.java
@@ -1,10 +1,14 @@
 package snippets.rest.ws.client;
 
 import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  * This is a simple test class for invoking REST web service
- * using JAX-RS clinet APIs
+ * using JAX-RS client APIs
  */
 public class CourseManagementClient {
 
@@ -16,10 +20,22 @@ public class CourseManagementClient {
     public static void testGetCourseJSON() {
         //Create JAX-RS client
         Client client = ClientBuilder.newClient();
+
         //Get WebTarget for a URL
         WebTarget webTarget = client.target("http://localhost:8080/CourseManagementREST/services/course");
         //Add paths to RUL
         webTarget = webTarget.path("get").path("10");
 
+        //Execute HTTP get method
+        Response response = webTarget.request(MediaType.APPLICATION_JSON).get();
+
+        //Check responses code. 200 is OK
+        if (response.getStatus() != 200) {
+            System.out.println("Error invoking REST Web  Service - " + response.getStatusInfo().getReasonPhrase());
+            return;
+        }
+
+        //REST call was successful. Print the response
+        System.out.println(response.readEntity(String.class));
     }
 }
