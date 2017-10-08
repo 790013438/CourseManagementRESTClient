079f191cd63edba23049e605da43b8ea03f9174b
790013438 <790013438@qq.com>
2017-10-08 11:04:46 +0800

    Writing a Java client for the REST POST web
    We need to send input data (the Course information) in the JSON format.
    Although we have hardcoded JSON in our example, you could use JAXB or
    any other library that converts a Java object into JSON.  Note that we
    have used the post method (webTarget.request().post(…)). We have also
    set the content type of the request to "application/JSON" (because our
    web service to add Course consumes the JSON format). This is done by
    creating an entity and setting its content type to JSON:

--- src/main/java/snippets/rest/ws/client/CourseManagementClient.java
+++ src/main/java/snippets/rest/ws/client/CourseManagementClient.java
@@ -2,6 +2,7 @@ package snippets.rest.ws.client;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -32,6 +33,37 @@ public class CourseManagementClient {
         //Check responses code. 200 is OK
         if (response.getStatus() != 200) {
             System.out.println("Error invoking REST Web  Service - " + response.getStatusInfo().getReasonPhrase());
+            return;
+        }
+
+        //REST call was successful. Print the response
+        System.out.println(response.readEntity(String.class));
+    }
+
+    //Test addCourse method (JSON version) of CourseService
+    public static void testAddCourseJSON() {
+
+        //Create JAX-RS client
+        Client client = ClientBuilder.newClient();
+
+        //Get WebTarget for a URL
+        WebTarget webTarget = client.target("http://localhost:8600/CourseManagementREST/services/course/add");
+
+        //Create JSON representation of Course,
+        //with course_name and credits fields. Instead of creating 
+        //JSON manually, you could also use JAXB to create JSON from 
+        //Java object.
+        String courseJSON = "{\"course_name\":\"Course-4\", \"credits\":5}";
+
+        //Execute HTTP post method
+        Response response = webTarget.request().post(Entity.entity(courseJSON, MediaType.APPLICATION_JSON_TYPE));
+
+        //Check response code. 200 is OK
+        if (response.getStatus() != 200) {
+            //Print error message
+            System.out.println("Error invoking REST Web Service - " + response.getStatusInfo().getReasonPhrase() + ", Error Code : " + response.getStatus());
+            //Also dump content of response message
+            System.out.println(response.readEntity(String.class));
             return;
         }
 
